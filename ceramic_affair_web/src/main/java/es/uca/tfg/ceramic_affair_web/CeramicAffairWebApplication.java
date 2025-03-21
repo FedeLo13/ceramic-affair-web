9963e6df0454d04bb876db329eee4a555d6f1f53
FedeLo <fede.lopezpe@alum.uca.es>
2025-03-21 18:30:07 +0100

    Added Maven proyect

---
+++ ceramic_affair_web/src/main/java/es/uca/tfg/ceramic_affair_web/CeramicAffairWebApplication.java
@@ -0,0 +1,13 @@
+package es.uca.tfg.ceramic_affair_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CeramicAffairWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CeramicAffairWebApplication.class, args);
+	}
+
+}
