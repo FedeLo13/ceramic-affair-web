c771292b5802e7ba62e670cb22c9fa89f68ba7a7
FedeLo <fede.lopezpe@alum.uca.es>
2025-04-13 11:19:19 +0200

    Añadido CategoriaService

---
+++ ceramic_affair_web/src/main/java/es/uca/tfg/ceramic_affair_web/services/CategoriaService.java
@@ -0,0 +1,89 @@
+package es.uca.tfg.ceramic_affair_web.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import es.uca.tfg.ceramic_affair_web.entities.Categoria;
+import es.uca.tfg.ceramic_affair_web.entities.Producto;
+import es.uca.tfg.ceramic_affair_web.repositories.CategoriaRepo;
+import es.uca.tfg.ceramic_affair_web.repositories.ProductoRepo;
+
+/**
+ * Servicio para la entidad Categoria.
+ * 
+ * @version 1.0
+ */
+@Service
+public class CategoriaService {
+
+    @Autowired
+    private CategoriaRepo categoriaRepo;
+
+    @Autowired
+    private ProductoRepo productoRepo;
+
+    /**
+     * Método para insertar una nueva categoría
+     * 
+     * @param categoria la categoría a insertar
+     */
+    public void insertarCategoria(Categoria categoria) {
+        categoriaRepo.save(categoria);
+    }
+
+    /**
+     * Método para obtener una categoría por su id
+     * 
+     * @param id el id de la categoría a obtener
+     */
+    public Categoria obtenerPorId(Long id) {
+        return categoriaRepo.findById(id)
+            .orElseThrow(() -> new IllegalArgumentException("Categoría no encontrada con id: " + id));
+    }
+
+    /**
+     * Método para eliminar una categoría
+     * 
+     * @param id el id de la categoría a eliminar
+     */
+    public void eliminarCategoria(Long id) {
+        Categoria categoria = categoriaRepo.findById(id)
+            .orElseThrow(() -> new IllegalArgumentException("Categoría no encontrada con id: " + id));
+        
+        // Desvincular todos los productos de la categoría antes de eliminarla
+        categoria.getProductos().forEach(producto -> {
+            producto.setCategoria(null);
+            productoRepo.save(producto);
+        });
+        // Eliminar la categoría
+        categoriaRepo.delete(categoria);
+    }
+
+    /**
+     * Método para obtener todas las categorías
+     * 
+     * @return una lista de todas las categorías
+     */
+    public List<Categoria> obtenerTodas() {
+        return categoriaRepo.findAll();
+    }
+
+    /**
+     * Método para eliminar todas las categorías
+     */
+    public void eliminarTodas() {
+        List<Producto> productos = productoRepo.findAll();
+
+        // Desvincular todos los productos de las categorías antes de eliminar
+        for(Producto producto : productos) {
+            if(producto.getCategoria() != null) {
+                producto.setCategoria(null);
+                productoRepo.save(producto);
+            }
+        }
+
+        categoriaRepo.deleteAll();
+    }
+}
