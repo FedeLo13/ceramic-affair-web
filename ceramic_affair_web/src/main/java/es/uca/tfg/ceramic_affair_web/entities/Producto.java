390688e10506d375a6c89efa93158e9b0a86fc3b
FedeLo <fede.lopezpe@alum.uca.es>
2025-04-13 11:45:39 +0200

    Añadida etiqueta a descripcion

--- ceramic_affair_web/src/main/java/es/uca/tfg/ceramic_affair_web/entities/Producto.java
+++ ceramic_affair_web/src/main/java/es/uca/tfg/ceramic_affair_web/entities/Producto.java
@@ -13,6 +13,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.PreRemove;
 
@@ -28,6 +29,7 @@ public class Producto {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nombre;
+    @Lob
     private String descripcion;
     private BigDecimal precio;
     private boolean soldOut;
