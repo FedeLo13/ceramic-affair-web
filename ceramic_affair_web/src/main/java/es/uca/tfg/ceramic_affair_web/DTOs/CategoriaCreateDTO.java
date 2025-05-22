a884ad8275928c845459c4662ae7f7de8cc71c61
FedeLo <fede.lopezpe@alum.uca.es>
2025-05-22 22:45:49 +0200

    Added DTO classes

---
+++ ceramic_affair_web/src/main/java/es/uca/tfg/ceramic_affair_web/DTOs/CategoriaCreateDTO.java
@@ -0,0 +1,14 @@
+package es.uca.tfg.ceramic_affair_web.DTOs;
+
+public class CategoriaCreateDTO {
+
+    private String nombre;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+}
