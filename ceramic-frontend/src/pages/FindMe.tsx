2d8e18e8e2c9bbb1ece91b8b32191aefef45a2b9
FedeLo <fede.lopezpe@alum.uca.es>
2025-05-30 12:09:23 +0200

    Updated FindMe page message

--- ceramic-frontend/src/pages/FindMe.tsx
+++ ceramic-frontend/src/pages/FindMe.tsx
@@ -2,12 +2,8 @@ export default function FindMe() {
     return ( 
         <div>
             <h1>Find Me</h1>
-            <p>Discover where you can find our ceramic pieces in person.</p>
-            <ul>
-                <li>Location 1: Address, City, State</li>
-                <li>Location 2: Address, City, State</li>
-                <li>Location 3: Address, City, State</li>
-            </ul>
+            <p> THIS PAGE IS UNDER CONSTRUCTION</p>
+            <p>We are working hard to bring you this feature. Stay tuned!</p>
             {/* Add more content or components related to finding locations here */}
         </div>
     );
