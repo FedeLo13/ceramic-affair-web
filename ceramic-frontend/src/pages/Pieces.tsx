b7b22b2f6292ec042947ddf1784ac64eecb55f07
FedeLo <fede.lopezpe@alum.uca.es>
2025-05-30 20:17:47 +0200

    Moved Pieces.tsx onto a folder and included css

--- ceramic-frontend/src/pages/Pieces.tsx
+++
@@ -1,9 +0,0 @@
-export default function Pieces() {
-    return (
-        <div>
-            <h1>Pieces</h1>
-            <p>Here you can find a collection of our ceramic pieces.</p>
-            {/* Add more content or components related to pieces here */}
-        </div>
-    );
-}
