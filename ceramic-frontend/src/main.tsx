14d4055980212ce11d2c3939973b06b6fce519aa
FedeLo <fede.lopezpe@alum.uca.es>
2025-07-03 12:42:20 +0200

    Added global.css and some font settings

--- ceramic-frontend/src/main.tsx
+++ ceramic-frontend/src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import './styles/reset.css'
 import './styles/variables.css'
+import './styles/global.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
