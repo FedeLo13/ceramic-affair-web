49cec8d70d747f44c06b3363cd1281b81b1ec96d
FedeLo <fede.lopezpe@alum.uca.es>
2025-07-09 14:06:43 +0200

    Updated file to accept prop config

--- ceramic_affair_web/src/main/java/es/uca/tfg/ceramic_affair_web/CeramicAffairWebApplication.java
+++ ceramic_affair_web/src/main/java/es/uca/tfg/ceramic_affair_web/CeramicAffairWebApplication.java
@@ -2,7 +2,11 @@ package es.uca.tfg.ceramic_affair_web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
+import es.uca.tfg.ceramic_affair_web.configuration.ImagenProperties;
+
+@EnableConfigurationProperties(ImagenProperties.class)
 @SpringBootApplication
 public class CeramicAffairWebApplication {
 
