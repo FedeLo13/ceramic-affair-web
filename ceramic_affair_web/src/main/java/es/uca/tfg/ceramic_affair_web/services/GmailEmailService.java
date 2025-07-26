dfcb45b86801ebc16268404ce645b31c30115b57
FedeLo <fede.lopezpe@alum.uca.es>
2025-07-26 12:30:10 +0200

    Added services to handle email notifications

---
+++ ceramic_affair_web/src/main/java/es/uca/tfg/ceramic_affair_web/services/GmailEmailService.java
@@ -0,0 +1,41 @@
+package es.uca.tfg.ceramic_affair_web.services;
+
+import org.springframework.context.annotation.Primary;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+
+import jakarta.mail.internet.MimeMessage;
+
+/**
+ * Implementación del servicio de envío de correos electrónicos utilizando Gmail.
+ * 
+ * @version 1.0
+ */
+@Service
+@Primary
+public class GmailEmailService implements EmailService {
+
+    private final JavaMailSender mailSender;
+
+    public GmailEmailService(JavaMailSender mailSender) {
+        this.mailSender = mailSender;
+    }
+
+    @Override
+    public void sendEmail(String to, String subject, String body) {
+        MimeMessage message = mailSender.createMimeMessage();
+
+        try {
+            MimeMessageHelper helper = new MimeMessageHelper(message, true, "UTF-8");
+            helper.setTo(to);
+            helper.setSubject(subject);
+            //helper.setFrom("correo@gmail.com", "Ceramic Affair");
+            helper.setText(body, true);
+
+            mailSender.send(message);
+        } catch (Exception e) {
+            throw new RuntimeException("Error al enviar el correo electrónico", e);
+        }
+    }
+}
