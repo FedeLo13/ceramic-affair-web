b9de48ba140bee8cae529910ac447829821dc7c6
FedeLo <fede.lopezpe@alum.uca.es>
2025-08-01 22:21:48 +0200

    Enabled asyncrony and scheduling

--- ceramic_affair_web/src/main/java/es/uca/tfg/ceramic_affair_web/CeramicAffairWebApplication.java
+++ ceramic_affair_web/src/main/java/es/uca/tfg/ceramic_affair_web/CeramicAffairWebApplication.java
@@ -3,11 +3,15 @@ package es.uca.tfg.ceramic_affair_web;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 import es.uca.tfg.ceramic_affair_web.configuration.ImagenProperties;
 
 @EnableConfigurationProperties(ImagenProperties.class)
 @SpringBootApplication
+@EnableAsync
+@EnableScheduling
 public class CeramicAffairWebApplication {
 
 	public static void main(String[] args) {
