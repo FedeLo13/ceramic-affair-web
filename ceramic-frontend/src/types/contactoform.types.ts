3aa98f8247f2dbc362e58b20f54a64a79fb03e27
FedeLo <fede.lopezpe@alum.uca.es>
2025-08-03 13:13:40 +0200

    Added the rest of the types

---
+++ ceramic-frontend/src/types/contactoform.types.ts
@@ -0,0 +1,8 @@
+export interface ContactoFormDTO {
+    nombre: string;
+    apellidos: string;
+    email: string;
+    asunto: string;
+    mensaje: string;
+    recaptcha: string;
+}
---
+++ ceramic-frontend/src/types/suscripcion.types.ts
@@ -0,0 +1,4 @@
+export interface SuscripcionDTO {
+    email: string;
+    recaptcha: string;
+}
