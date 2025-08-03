d9ada68d9426e640578a6e9e0bcf06159d49069d
FedeLo <fede.lopezpe@alum.uca.es>
2025-08-03 13:11:48 +0200

    Updated categoria and producto types

--- ceramic-frontend/src/types/producto.types.ts
+++ ceramic-frontend/src/types/producto.types.ts
@@ -1,4 +1,4 @@
-import type { Categoria } from "./categoria.types";
+import type { Imagen } from "./imagen.types";
 
 export interface Producto {
   id: number;
@@ -6,15 +6,15 @@ export interface Producto {
   descripcion: string;
   precio: number;
   soldOut: boolean;
-  categoria: Categoria | null;
+  categoria: string
   altura: number;
   anchura: number;
   diametro: number;
   fechaCreacion: string; // ISO string (ej. "2024-05-22T14:00:00")
-  imagenes: string[];
+  imagenes: Imagen[]; // Array de objetos Imagen
 }
 
-export interface ProductoInput {
+export interface ProductoInputDTO {
   nombre: string;
   descripcion: string;
   precio: number;
@@ -23,11 +23,22 @@ export interface ProductoInput {
   altura: number;
   anchura: number;
   diametro: number;
-  imagenes: string[];
+  imagenes: number[];
 }
 
-export interface ProductoCreateDTO {
+export interface ProductoOutputDTO {
+  id: number;
   nombre: string;
   descripcion: string;
   precio: number;
+  soldOut?: boolean;
+  categoriaId: number;
+  altura: number;
+  anchura: number;
+  diametro: number;
+  imagenes: number[];
+}
+
+export interface ProductoStockDTO {
+  soldOut: boolean;
 }
