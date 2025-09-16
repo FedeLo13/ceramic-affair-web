bdc1b9f5db382c2f428248ccbe972c93950a56e6
FedeLo <fede.lopezpe@alum.uca.es>
2025-09-16 12:47:42 +0200

    Added comment for business exception

--- ceramic_affair_web/src/main/java/es/uca/tfg/ceramic_affair_web/exceptions/BusinessException.java
+++ ceramic_affair_web/src/main/java/es/uca/tfg/ceramic_affair_web/exceptions/BusinessException.java
@@ -2,6 +2,12 @@ package es.uca.tfg.ceramic_affair_web.exceptions;
 
 import org.springframework.http.HttpStatus;
 
+/**
+ * Clase base para manejar excepciones de negocio en la aplicación.
+ * Extiende RuntimeException para permitir excepciones no verificadas.
+ * 
+ * @version 1.0
+ */
 public class BusinessException extends RuntimeException {
 
     private final HttpStatus status;
